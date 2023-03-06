fcbca4af02c483669235841652951025bb689f99
Mohdarif Khan (ext) <mohdarif.khan@henkel.com>
2023-03-06 06:57:52 +0100

    Add existing project files to Git

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'ApplicationUsers';
+
+  ngOnInit() {
+  //   $('#sidebarCollapse').on('click', function () {
+  //     $('#sidebar').toggleClass('active');
+  // });
+  }
+ 
 }
